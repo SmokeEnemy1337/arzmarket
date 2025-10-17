a29bbdb52f1f1b72b08fd14078e6272faaf5a507
dryger1337 <chesnoc1897@gmail.com>
2025-10-17 13:38:14 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 {
-    "updateurl": "https://github.com/SmokeEnemy1337/arzmarket/raw/refs/heads/main/%23ArzMarket.lua",
+    "updateurl": "https://github.com/SmokeEnemy1337/arzmarket/raw/refs/heads/main/%23ArzMarket%5B3.28.1%5D.lua",
     "latest": "3.29"
 }
